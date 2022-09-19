5c6a078ecda8e1a56e14853dd4b0084b3e5975e5
Pedro Cobos <pedro.cobos@live.com>
2022-09-19 20:23:18 -0300

    installed dependencies

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import './styles/App.css';
-import twitterLogo from './assets/twitter-logo.svg';
+import "./styles/App.css";
+import twitterLogo from "./assets/twitter-logo.svg";
 import React from "react";
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = "_buildspace";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
-const OPENSEA_LINK = '';
+const OPENSEA_LINK = "";
 const TOTAL_MINT_COUNT = 50;
 
 const App = () => {
+  const checkIfWalletIsConnected = () => {
+    /*
+     * First make sure we have access to window.ethereum
+     */
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("Make sure you have metamask!");
+      return;
+    } else {
+      console.log("We have the ethereum object", ethereum);
+    }
+  };
+
   // Render Methods
   const renderNotConnectedContainer = () => (
     <button className="cta-button connect-wallet-button">
